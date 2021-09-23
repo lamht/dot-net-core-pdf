dff3e808aac1e4b9e0aa414171b3793cf615f751
Lam Huynh <thanhlam0290@gmail.com>
2021-09-23 13:05:39 +0700

    init

---
+++ ConsoleApp1/Pdf.cs
@@ -0,0 +1,43 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Pdf
+    {
+        IConverter _converter;
+        public Pdf(IConverter converter)
+        {
+            _converter = converter;
+        }
+        public byte[] ConvertPdf(string htmlContent)
+        {
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+
+                    Margins = { Top = 10, Bottom = 10, Left = 20, Right = 20, Unit = DinkToPdf.Unit.Millimeters },
+                    //Out = Path.Combine(Directory.GetCurrentDirectory(), "assets", "test.pdf")
+                },
+            };
+            var page = new ObjectSettings()
+            {
+                PagesCount = true,
+                WebSettings = { DefaultEncoding = "utf-8" },
+                HtmlContent = htmlContent
+            };
+            pdf.Objects.Add(page);
+            return _converter.Convert(pdf);
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp1;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //setup our DI
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddSingleton<Pdf, Pdf>()
+                .AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()))
+                .BuildServiceProvider();
+
+            //configure console logging
+            //serviceProvider
+            //    .GetService<ILoggerFactory>()
+            //    .AddConsole(LogLevel.Debug);
+
+            Console.WriteLine("Hello, World!");
+            var pdf = serviceProvider.GetService<Pdf>(); ;
+            string html = File.ReadAllText("invoice.html");
+            var result = pdf.ConvertPdf(html);
+            string pathDir = Path.Combine(Directory.GetCurrentDirectory(),"pdf");
+            string path = Path.Combine(pathDir, $"invoice_{DateTime.UtcNow:yyyyMMddHHmmss}.pdf");
+            if (!Directory.Exists(pathDir))
+            {
+                Directory.CreateDirectory(pathDir);
+            }
+            File.WriteAllBytes(path, result);
+            Console.WriteLine(path);
+        }
+        
+    }
+}
