16ec0fe7b2cb2dc7263aeb81af5b4fee7c2ebe24
Lam Huynh <thanhlam0290@gmail.com>
2021-09-27 16:45:00 +0700

    fix load css from url

--- ConsoleApp1/Pdf.cs
+++ ConsoleApp1/Pdf.cs
@@ -24,20 +24,54 @@ namespace ConsoleApp1
                     ColorMode = ColorMode.Color,
                     Orientation = Orientation.Portrait,
                     PaperSize = PaperKind.A4,
-
-                    Margins = { Top = 10, Bottom = 10, Left = 20, Right = 20, Unit = DinkToPdf.Unit.Millimeters },
-                    //Out = Path.Combine(Directory.GetCurrentDirectory(), "assets", "test.pdf")
+                    Margins = { Top = 70, Bottom = 25, Left = 20, Right = 20, Unit = DinkToPdf.Unit.Millimeters }
                 },
             };
+
+            string headerPath = GetHeaderPath();
+            string footerPath = GetFooterPath();
             var page = new ObjectSettings()
             {
                 PagesCount = true,
                 WebSettings = { DefaultEncoding = "utf-8" },
-                HtmlContent = htmlContent
+                HtmlContent = htmlContent,
+                HeaderSettings = { HtmUrl = headerPath },
+                FooterSettings = { HtmUrl = footerPath }
             };
             pdf.Objects.Add(page);
-            return _converter.Convert(pdf);
-            
+            byte[] data = _converter.Convert(pdf);
+            File.Delete(headerPath);
+            File.Delete(footerPath);
+            return data;
+        }
+
+        public string GetHeaderPath()
+        {
+            string html = File.ReadAllText("header.html");
+            //html.Replace
+            string pathDir = Path.Combine(Directory.GetCurrentDirectory(), "html");
+            string path = Path.Combine(pathDir, $"header_{DateTime.UtcNow:yyyyMMddHHmmss}.html");
+            if (!Directory.Exists(pathDir))
+            {
+                Directory.CreateDirectory(pathDir);
+            }
+            File.WriteAllText(path, html);
+
+            return path;
+        }
+
+        public string GetFooterPath()
+        {
+            string html = File.ReadAllText("footer.html");
+            string pathDir = Path.Combine(Directory.GetCurrentDirectory(), "html");
+            string path = Path.Combine(pathDir, $"footer_{DateTime.UtcNow:yyyyMMddHHmmss}.html");
+            if (!Directory.Exists(pathDir))
+            {
+                Directory.CreateDirectory(pathDir);
+            }
+            File.WriteAllText(path, html);
+
+            return path;
         }
     }
 }
